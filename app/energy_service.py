d8f609bf6c71e088ee0be093ea126df05d0211f5
Dylan Bolger <dylan.bolger00@gmail.com>
2023-10-15 13:14:35 -0500

    Add debugfile logic to limit actual calls to energy provider

--- app/energy_service.py
+++ app/energy_service.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.login.login import login
 from app.energy_request.energy_request import request
+import os
+import json
 
 app = FastAPI(docs_url=None, redoc_url=None)
+debugFilePath = "./get_response.json"
 
 @app.get('/get')
 def get():
+    if (debugFileExists()):
+        print('Debug file present')
+        return getDebugResponse()
     sessionKeys = login()
     return request(sessionKeys)
+
+def getDebugResponse():
+    with open(debugFilePath) as file:
+        return json.load(file)
+
+def debugFileExists():
+    return os.path.isfile(debugFilePath)
